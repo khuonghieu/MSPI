45521e388472ebecd4fe15fcc331a22fa1b20daa
Hieu Khuong <tug60121@temple.edu>
2019-10-13 16:18:07 -0400

    Google LogIn Added

--- app/src/main/java/edu/temple/spiapp/MainActivity.java
+++ app/src/main/java/edu/temple/spiapp/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    //test commit, making sure I have version control setup properly.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
