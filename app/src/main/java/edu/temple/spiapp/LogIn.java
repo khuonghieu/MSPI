e99c3f1191f3f98f39c436dc11ec332c543518c3
Hieu Khuong <tug60121@temple.edu>
2019-10-15 01:08:21 -0400

    Added Github login

--- app/src/main/java/edu/temple/spiapp/LogIn.java
+++ app/src/main/java/edu/temple/spiapp/LogIn.java
@@ -3,23 +3,28 @@ package edu.temple.spiapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
+
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import java.util.Arrays;
-import java.util.Collections;
+import com.google.firebase.auth.OAuthProvider;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -27,19 +32,100 @@ public class LogIn extends AppCompatActivity {
 
     GoogleSignInClient mGoogleSignInClient;
     int RC_SIGN_IN = 0;
+    Button googleLogIn;
+    Button githubLogIn;
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        //Google
+        final GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
+        googleLogIn = findViewById(R.id.googleLogIn);
+        googleLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleSignIn();
+            }
+        });
+
+        //Github
+
+        githubLogIn = findViewById(R.id.githubLogIn);
+        githubLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OAuthProvider.Builder provider = OAuthProvider.newBuilder("github.com");
+                List<String> scopes =
+                        new ArrayList<String>() {
+                            {
+                                add("user:email");
+                            }
+                        };
+
+                Task<AuthResult> pendingResultTask = mAuth.getPendingAuthResult();
+
+                if (pendingResultTask != null) {
+                    // There's something already here! Finish the sign-in for your user.
+                    pendingResultTask
+                            .addOnSuccessListener(
+                                    new OnSuccessListener<AuthResult>() {
+                                        @Override
+                                        public void onSuccess(AuthResult authResult) {
+                                            Intent intent = new Intent(LogIn.this, MainActivity.class);
+                                            intent.putExtra("Service name", "Github");
+                                            intent.putExtra("accountName", authResult.getUser()
+                                                    .getDisplayName());
+                                            startActivity(intent);
+                                        }
+                                    })
+                            .addOnFailureListener(
+                                    new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(LogIn.this, "Failed github",
+                                                    Toast.LENGTH_LONG).show();
+                                        }
+                            });
+                } else {
+                    provider.setScopes(scopes);
+                    mAuth.startActivityForSignInWithProvider(LogIn.this, provider.build())
+                        .addOnSuccessListener(
+                                new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Intent intent = new Intent(LogIn.this, MainActivity.class);
+                                        intent.putExtra("Service name","Github");
+                                        intent.putExtra("accountName", authResult.getUser()
+                                                .getDisplayName());
+                                        startActivity(intent);
+                                    }
+                                })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(LogIn.this, "Failed github",
+                                                Toast.LENGTH_LONG).show();
+                                    }
+                        });
+                }
+            }
+        });
 
     }
-    private void signIn() {
+    private void googleSignIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
@@ -60,27 +146,32 @@ public class LogIn extends AppCompatActivity {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
             // Signed in successfully, show authenticated UI.
             Intent intent = new Intent(LogIn.this, MainActivity.class);
+            intent.putExtra("Service name", "Google");
             intent.putExtra("accountName", account.getDisplayName());
             startActivity(intent);
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
             Log.w("Google Sign In Error", "signInResult:failed code=" + e.getStatusCode());
-            Toast.makeText(LogIn.this, "Failed", Toast.LENGTH_LONG).show();
+            Toast.makeText(LogIn.this, "Failed google", Toast.LENGTH_LONG).show();
         }
     }
 
     @Override
-    protected void onStart() {
-        // Check for existing Google Sign In account, if the user is already signed in
-        // the GoogleSignInAccount will be non-null.
-
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if (account != null) {
-            startActivity(new Intent(LogIn.this, LogIn2.class));
+    public void onStart(){
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        GoogleSignInAccount currentGoogleUser = GoogleSignIn.getLastSignedInAccount(this);
+        if(currentUser!=null){
+            startActivity(new Intent(LogIn.this,MainActivity.class)
+                    .putExtra("accountName",currentUser.getDisplayName())
+                    .putExtra("Service name","Github"));
+        }
+        else if (currentGoogleUser!=null){
+            startActivity(new Intent(LogIn.this,MainActivity.class)
+                    .putExtra("accountName", currentGoogleUser.getDisplayName())
+                    .putExtra("Service name","Google"));
         }
         super.onStart();
     }
-
-
 }
--- app/src/main/java/edu/temple/spiapp/MainActivity.java
+++ app/src/main/java/edu/temple/spiapp/MainActivity.java
@@ -1,13 +1,23 @@
 package edu.temple.spiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button signoutButton;
     //test commit, making sure I have version control setup properly.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button chooseCam = findViewById(R.id.chooseCam);
         TextView userName = findViewById(R.id.userText);
-        userName.setText("Logged in as:" + getIntent().getStringExtra("accountName"));
+        userName.setText(getIntent().getStringExtra("Service name")+":"+ getIntent().getStringExtra("accountName"));
         chooseCam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,6 +36,26 @@ public class MainActivity extends AppCompatActivity {
                 viewCam.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
                 viewCam.loadUrl(url);
 
+            }
+        });
+        signoutButton = findViewById(R.id.signoutButton);
+        signoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                        .build();
+                GoogleSignInAccount googleCurrentAcc = GoogleSignIn.getLastSignedInAccount(MainActivity.this);
+                FirebaseUser githubCurrentAcc = FirebaseAuth.getInstance().getCurrentUser();
+                if(googleCurrentAcc!=null){
+                    GoogleSignIn.getClient(MainActivity.this,gso).signOut();
+                    startActivity(new Intent(MainActivity.this,LogIn.class));
+                } else if(githubCurrentAcc!=null){
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(MainActivity.this,LogIn.class));
+                }
+                finish();
             }
         });
     }
