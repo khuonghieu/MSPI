d12e4b8b5534530985953e64f110b61ed0b78bf8
Hieu Khuong <tug60121@temple.edu>
2019-11-02 17:15:10 -0400

    Show 1 video player for better performance

--- app/src/main/java/edu/temple/spiapp/VideoAdapter.java
+++ app/src/main/java/edu/temple/spiapp/VideoAdapter.java
@@ -2,19 +2,14 @@ package edu.temple.spiapp;
 
 import android.content.Context;
 import android.net.Uri;
-import android.view.SurfaceView;
-import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.MediaController;
 import android.widget.TextView;
-import android.widget.VideoView;
 
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.ProgressiveMediaSource;
@@ -22,7 +17,6 @@ import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -31,16 +25,18 @@ public class VideoAdapter extends BaseAdapter {
     public Context context;
     public ArrayList<Uri> videoLinkArray;
     public ArrayList<String> videoNameArray;
+    public PlayerView playerView;
 
-    public VideoAdapter(Context context, ArrayList<Uri> videoLinkArray, ArrayList<String> videoNameArray) {
+    public VideoAdapter(Context context, ArrayList<Uri> videoLinkArray, ArrayList<String> videoNameArray, PlayerView playerView) {
         this.context = context;
         this.videoLinkArray = videoLinkArray;
         this.videoNameArray = videoNameArray;
+        this.playerView = playerView;
     }
 
     @Override
     public int getCount() {
-        return Math.min(videoLinkArray.size(),2);
+        return Math.min(videoLinkArray.size(),3);
     }
 
     @Override
@@ -56,29 +52,13 @@ public class VideoAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(this.context);
 
         LinearLayout linearLayout = new LinearLayout(this.context);
         linearLayout.setOrientation(LinearLayout.VERTICAL);
 
-        PlayerView playerView = new PlayerView(this.context);
-        playerView.setPlayer(player);
-        playerView.setLayoutParams(new PlayerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,1200));
-
-        // Produces DataSource instances through which media data is loaded.
-        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(context,
-                Util.getUserAgent(context, "yourApplicationName"));
-        // This is the MediaSource representing the media to be played.
-        MediaSource videoSource = new ProgressiveMediaSource.Factory(dataSourceFactory)
-                .createMediaSource(videoLinkArray.get(videoLinkArray.size()-position-1));
-        // Prepare the player with the source.
-        player.prepare(videoSource);
-        player.setPlayWhenReady(false);
-
         TextView textView = new TextView(this.context);
         textView.setText(videoNameArray.get(videoNameArray.size() - position -1));
-
-        linearLayout.addView(playerView);
+        textView.setTextSize(20);
         linearLayout.addView(textView);
 
         return linearLayout;
--- app/src/main/java/edu/temple/spiapp/VideoFragment.java
+++ app/src/main/java/edu/temple/spiapp/VideoFragment.java
@@ -1,17 +1,23 @@
 package edu.temple.spiapp;
 
-import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -28,8 +34,10 @@ public class VideoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_video, container, false);
+
         final ListView videoList = view.findViewById(R.id.videoList);
         final ArrayList<Uri> videoUriList = new ArrayList<>();
+        final PlayerView playerView = view.findViewById(R.id.playerView);
         final ArrayList<String> videoNameList = new ArrayList<>();
         FirebaseStorage storage = FirebaseStorage.getInstance();
         StorageReference listRef = storage.getReferenceFromUrl("gs://mspi-a4b75.appspot.com/videos");
@@ -47,7 +55,7 @@ public class VideoFragment extends Fragment {
                     }).addOnCompleteListener(new OnCompleteListener<Uri>() {
                         @Override
                         public void onComplete(@NonNull Task<Uri> task) {
-                            VideoAdapter videoAdapter = new VideoAdapter(getContext(),videoUriList,videoNameList);
+                            VideoAdapter videoAdapter = new VideoAdapter(getContext(),videoUriList,videoNameList, playerView);
                             videoList.setAdapter(videoAdapter);
                         }
                     });
