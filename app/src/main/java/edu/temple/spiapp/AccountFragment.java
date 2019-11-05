4c614c14eb368adb7df6a100fa4700655a146fb8
Hieu Khuong <tug60121@temple.edu>
2019-11-04 23:14:25 -0500

    Changed the user profile page

--- app/src/main/java/edu/temple/spiapp/AccountFragment.java
+++ app/src/main/java/edu/temple/spiapp/AccountFragment.java
@@ -1,11 +1,13 @@
 package edu.temple.spiapp;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,6 +19,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
 public class AccountFragment extends Fragment {
     private final static String TAG = "AccountFragment";
@@ -26,11 +29,28 @@ public class AccountFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.account_fragment, container, false);
-        TextView userName = view.findViewById(R.id.userText);
-        userName.setText("Logged In by: " +
-                getActivity().getIntent().getStringExtra("Service name") + ":" + "\n"+
-                getActivity().getIntent().getStringExtra("accountName"));
 
+        GoogleSignInAccount googleCurrentAcc = GoogleSignIn.getLastSignedInAccount(getContext());
+        FirebaseUser githubCurrentAcc = FirebaseAuth.getInstance().getCurrentUser();
+
+        ImageView userAva = view.findViewById(R.id.userAva);
+        ImageView serviceIcon = view.findViewById(R.id.serviceIcon);
+        TextView userName = view.findViewById(R.id.userName);
+        TextView userEmail = view.findViewById(R.id.userEmail);
+
+        if(googleCurrentAcc!=null){
+            Picasso.get().load(googleCurrentAcc.getPhotoUrl()).into(userAva);
+            serviceIcon.setBackground(getResources().getDrawable(R.drawable.fui_ic_googleg_color_24dp, null));
+            userName.setText(googleCurrentAcc.getDisplayName());
+            userEmail.setText(googleCurrentAcc.getEmail());
+        }
+        else if(githubCurrentAcc!=null){
+            Picasso.get().load(githubCurrentAcc.getPhotoUrl()).into(userAva);
+            serviceIcon.setBackground(getResources().getDrawable(R.drawable.fui_ic_github_white_24dp,null));
+            serviceIcon.setBackgroundTintList(getResources().getColorStateList(R.color.fui_bgGitHub,null));
+            userName.setText(githubCurrentAcc.getDisplayName());
+            userEmail.setText(githubCurrentAcc.getEmail());
+        }
         signoutButton = view.findViewById(R.id.signoutButton);
         signoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
