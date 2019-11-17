c396c4eeb452579a1acfaa8befcc30f2e9005cea
Gerard Gibbons <MightyFineGoat@hotmail.com>
2019-11-17 13:05:47 -0500

    Implemented rest of Sign Up Activity

--- app/src/main/java/edu/temple/spiapp/SignUp.java
+++ app/src/main/java/edu/temple/spiapp/SignUp.java
@@ -1,14 +1,20 @@
 package edu.temple.spiapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
@@ -28,6 +34,41 @@ public class SignUp extends AppCompatActivity {
         pswd = findViewById(R.id.signUpPassword);
         pswd2 = findViewById(R.id.signUpPassword2);
         btnSignUp = findViewById(R.id.signUpButton);
+
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailId = emailID.getText().toString();
+                String paswd = pswd.getText().toString();
+                if (emailId.isEmpty()) {
+                    emailID.setError("Provide your Email first!");
+                    emailID.requestFocus();
+                } else if (paswd.isEmpty()) {
+                    pswd.setError("Set your password");
+                    pswd.requestFocus();
+                } else if (emailId.isEmpty() && paswd.isEmpty()) {
+                    Toast.makeText(SignUp.this, "Fields Empty!", Toast.LENGTH_SHORT).show();
+                } else if (!(emailId.isEmpty() && paswd.isEmpty())) {
+                    firebaseAuth.createUserWithEmailAndPassword(emailId, paswd).addOnCompleteListener(SignUp.this, new OnCompleteListener() {
+
+                        @Override
+                        public void onComplete(@NonNull Task task) {
+
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(SignUp.this.getApplicationContext(),
+                                        "SignUp unsuccessful: " + task.getException().getMessage(),
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                startActivity(new Intent(SignUp.this, MainActivity.class));
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(SignUp.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 
     }
